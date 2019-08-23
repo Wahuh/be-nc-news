0bb81905576c163be13db49e020bc8e55f22ec1e
Thanh <tmdoan98@gmail.com>
2019-08-23 13:02:25 +0100

    remove Promise from parameters so it uses native JS promises

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -2,17 +2,18 @@ const {
   topicData,
   articleData,
   commentData,
-  userData,
-} = require('../data/index.js');
+  userData
+} = require("../data/index.js");
 
-const { formatDates, formatComments, makeRefObj } = require('../utils/utils');
+const { formatDates, formatComments, makeRefObj } = require("../utils/utils");
 
-exports.seed = function(knex, Promise) {
-  const topicsInsertions = knex('topics').insert(topicData);
-  const usersInsertions = knex('users').insert(userData);
+exports.seed = function(knex) {
+  const topicsInsertions = knex("topics").insert(topicData);
+  const usersInsertions = knex("users").insert(userData);
 
   return Promise.all([topicsInsertions, usersInsertions])
-    .then(() => {
+    .then(data => {
+      console.log(data);
       /* 
       
       Your article data is currently in the incorrect format and will violate your SQL schema. 
@@ -34,6 +35,6 @@ exports.seed = function(knex, Promise) {
 
       const articleRef = makeRefObj(articleRows);
       const formattedComments = formatComments(commentData, articleRef);
-      return knex('comments').insert(formattedComments);
+      return knex("comments").insert(formattedComments);
     });
 };
