dc34911128a04a048a8990657d51f906ac6cf7b0
Thanh <tmdoan98@gmail.com>
2019-08-27 09:47:41 +0100

    add catch block

--- controllers/topics-controller.js
+++ controllers/topics-controller.js
@@ -1,9 +1,11 @@
 const { selectTopics } = require("../models/topics-model");
 
 const getTopics = (req, res, next) => {
-  selectTopics().then(topics => {
-    res.status(200).json({ topics });
-  });
+  selectTopics()
+    .then(topics => {
+      res.status(200).json({ topics });
+    })
+    .catch(next);
 };
 
 module.exports = { getTopics };
