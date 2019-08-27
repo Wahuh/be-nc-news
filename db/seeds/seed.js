e80906998aa4462d1e6b083935a48efeefc6a6bf
Thanh <tmdoan98@gmail.com>
2019-08-27 11:47:31 +0100

    migrate and rollback then insert the data

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -3,26 +3,31 @@ const { topicData, articleData, commentData, userData } = require("../data");
 const { formatDates, formatComments, makeRefObj } = require("../utils/utils");
 
 exports.seed = function(knex) {
-  const topicsInsertions = knex
-    .insert(topicData)
-    .into("topics")
-    .returning("*");
-  const usersInsertions = knex
-    .insert(userData)
-    .into("users")
-    .returning("*");
-
-  return Promise.all([topicsInsertions, usersInsertions])
-    .then(([topics, users]) => {
-      const formattedArticleData = formatDates(articleData);
-      return knex
-        .insert(formattedArticleData)
-        .into("articles")
+  return knex.migrate
+    .rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => {
+      const topicsInsertions = knex
+        .insert(topicData)
+        .into("topics")
         .returning("*");
-    })
-    .then(articleRows => {
-      const articleRef = makeRefObj(articleRows);
-      const formattedComments = formatComments(commentData, articleRef);
-      return knex("comments").insert(formattedComments);
+      const usersInsertions = knex
+        .insert(userData)
+        .into("users")
+        .returning("*");
+
+      return Promise.all([topicsInsertions, usersInsertions])
+        .then(([topics, users]) => {
+          const formattedArticleData = formatDates(articleData);
+          return knex
+            .insert(formattedArticleData)
+            .into("articles")
+            .returning("*");
+        })
+        .then(articleRows => {
+          const articleRef = makeRefObj(articleRows);
+          const formattedComments = formatComments(commentData, articleRef);
+          return knex("comments").insert(formattedComments);
+        });
     });
 };
