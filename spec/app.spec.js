583c417efe3b38f309c320dbff8f8fbcb8473a2f
Thanh <tmdoan98@gmail.com>
2019-08-27 09:38:29 +0100

    /api/users/:username GET happy path

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -10,7 +10,7 @@ describe("/api", () => {
 
   describe("/topics", () => {
     describe("GET", () => {
-      it("status 200: returns an object with a topics key containing array of topic objects with slug and description properties", () => {
+      it("status 200: returns an object with a topics key containing an array of objects with slug and description properties", () => {
         return request(app)
           .get("/api/topics")
           .expect(200)
@@ -18,6 +18,20 @@ describe("/api", () => {
             const { topics } = response.body;
             const [topic] = topics;
             expect(topic).to.have.all.keys(["slug", "description"]);
+          });
+      });
+    });
+  });
+
+  describe("/users", () => {
+    describe("/:username", () => {
+      it("status 200: returns an object with a user key containing an object with username, avatar_url and name properties", () => {
+        return request(app)
+          .get("/api/users/lurker")
+          .expect(200)
+          .then(response => {
+            const { user } = response.body;
+            expect(user).to.have.all.keys(["username", "avatar_url", "name"]);
           });
       });
     });
