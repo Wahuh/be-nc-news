d1bcc297edba33a8540d1afa5df1e6d561e08e81
Thanh <tmdoan98@gmail.com>
2019-08-28 10:08:03 +0100

    handle 422 errors

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routes/api-router");
-const { handle400Errors, handle404Errors } = require("./errors");
+const {
+  handle400Errors,
+  handle404Errors,
+  handle422Errors
+} = require("./errors");
 
 app.use(express.json());
 app.use("/api", apiRouter);
 app.use(handle400Errors);
 app.use(handle404Errors);
+app.use(handle422Errors);
 
 module.exports = app;
--- errors/index.js
+++ errors/index.js
@@ -9,10 +9,26 @@ const handle400Errors = (err, req, res, next) => {
 };
 
 const handle404Errors = (err, req, res, next) => {
-  const { status, msg } = err;
+  const codes = ["23503"];
+  const { status, msg, code } = err;
+  // if (codes.includes(code)) {
+  //   res.status(status).json({ msg: err.message });
+  // }
   if (status === 404) {
     res.status(status).json({ msg });
+  } else {
+    next(err);
   }
 };
 
-module.exports = { handle404Errors, handle400Errors };
+const handle422Errors = (err, req, res, next) => {
+  const codes = ["23503"];
+  const { status, msg, code } = err;
+  if (codes.includes(code)) {
+    res.status(422).json({ msg: "Unprocessable entity" });
+  } else {
+    next(err);
+  }
+};
+
+module.exports = { handle404Errors, handle400Errors, handle422Errors };
