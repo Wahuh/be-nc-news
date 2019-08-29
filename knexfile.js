e4354b3d181afa326cb8ad389bf9fef47b3a2f66
Thanh <tmdoan98@gmail.com>
2019-08-29 10:53:53 +0100

    knexfile.js needed for deployment, put password and username in dbDetails.js

---
+++ knexfile.js
@@ -0,0 +1,35 @@
+const { DB_URL } = process.env;
+const ENV = process.env.NODE_ENV || "development";
+const { username, password } = require('./dbDetails');
+
+const baseConfig = {
+  client: "pg",
+  migrations: {
+    directory: "./db/migrations"
+  },
+  seeds: {
+    directory: "./db/seeds"
+  }
+};
+
+const customConfig = {
+  development: {
+    connection: {
+      database: "nc_news",
+      username,
+      password
+    }
+  },
+  test: {
+    connection: {
+      database: "nc_news_test",
+      username,
+      password
+    }
+  },
+  production: {
+    connection: `${DB_URL}?ssl=true`
+  }
+};
+
+module.exports = { ...customConfig[ENV], ...baseConfig };
