c6e83c5939287cbc241b8378d5edd92a242b13ad
Thanh <tmdoan98@gmail.com>
2019-08-29 16:04:18 +0100

    add cascade delete to article_id on comments table

--- db/migrations/20190823125059_create_comments_table.js
+++ db/migrations/20190823125059_create_comments_table.js
@@ -11,7 +11,8 @@ exports.up = function(knex) {
     commentsTable
       .integer("article_id")
       .references("articles.article_id")
-      .notNullable();
+      .notNullable()
+      .onDelete("CASCADE");
     commentsTable.integer("votes").defaultTo(0);
     commentsTable.timestamp("created_at").defaultTo(knex.fn.now());
     commentsTable.string("body", 10000).notNullable();
